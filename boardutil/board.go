6b937706c03a450377e0afe977f45ea5bbe871e8
P Chan <shiroyamaryu@gmail.com>
2016-03-05 08:25:32 +0900

    Initial commit of boardutil

---
+++ boardutil/board.go
@@ -0,0 +1,56 @@
+// Package boardutil contains utility function for working with a single Board
+package boardutil
+
+import (
+	"fmt"
+)
+
+type Board struct {
+	// Each board is a 3x3 board
+	NW byte
+	N  byte
+	NE byte
+	W  byte
+	C  byte
+	E  byte
+	SW byte
+	S  byte
+	SE byte
+}
+
+func NewBoard() Board {
+	var board Board
+	board.NW = byte(' ')
+	board.N = byte(' ')
+	board.NE = byte(' ')
+	board.W = byte(' ')
+	board.C = byte(' ')
+	board.E = byte(' ')
+	board.SW = byte(' ')
+	board.S = byte(' ')
+	board.SE = byte(' ')
+	return board
+}
+
+func (board Board) PrintBoard() {
+	fmt.Printf(" %s | %s | %s \n", string(board.NW), string(board.N), string(board.NE))
+	fmt.Println("---+---+---")
+	fmt.Printf(" %s | %s | %s \n", string(board.W), string(board.C), string(board.E))
+	fmt.Println("---+---+---")
+	fmt.Printf(" %s | %s | %s \n", string(board.SW), string(board.S), string(board.SE))
+}
+
+// Plays a move by [player] at [position]
+// Returns: whether that is a legal move or not
+// e.g. board.Play("N", "X")
+//      board.Play("SW", "O")
+//      board.Play("SW", "X") -> false
+func (board Board) Play(position string, player byte) bool {
+	// TODO: make position into an enum
+	switch position {
+	case "N":
+		// TODO: check if the position is already taken
+		board.N = player
+	}
+	return true
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/pichangetheworld/supertictactoe/boardutil"
+)
+
+func main() {
+	board := boardutil.NewBoard()
+	board.PrintBoard()
+}
