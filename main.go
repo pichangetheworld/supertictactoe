40c98eb53e24d6baecaf5779a4f0fa182f7a5aa1
P Chan <shiroyamaryu@gmail.com>
2016-03-08 08:00:30 +0900

    Argument parsing and error handling

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 // Main function
@@ -21,17 +22,40 @@ func main() {
 		// read one line from stdin
 		line := scanner.Text()
 
-		// convert that line to an integer, err if not int
-		if v, err := strconv.Atoi(line); err == nil {
-			if v > 0 && v <= 9 {
-				if Play(getPosFromInt(v)) {
-					break
-				}
-			}
-		} else {
-			fmt.Print("Please enter a valid play")
+		// parse arguments
+		args := strings.Split(line, " ")
+
+		// whether the arguments are valid so far
+		valid := true
+
+		if len(args) == 0 {
+			valid = false
 		}
+
+		if valid {
+			// convert that line to an integer, err if not int
+			if v, err := strconv.Atoi(line); err == nil {
+				if v > 0 && v <= 9 {
+					if Play(getPosFromInt(v)) {
+						break
+					}
+				} else {
+					valid = false
+				}
+			} else {
+				valid = false
+			}
+		}
+
+		if !valid {
+			printPrompt()
+		}
+
 		fmt.Print("Enter your play (Ctrl+D to exit): ")
 	}
 	fmt.Println()
+}
+
+func printPrompt() {
+	fmt.Println("Please enter a valid play")
 }
